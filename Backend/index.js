9daf6afe845e5de9f2ac5ab9dace44bafbb81a02
Ejaz47-Hub <ejaz47026@gmail.com>
2025-09-06 08:14:20 +0530

    Instagram

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+import express, { urlencoded } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config({});
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    return res.status(200).json({
+        message : "hello nigga",
+        success:true
+    })
+})
+app.use(express.json())
+app.use(cookieParser())
+app.use(urlencoded({extended:true}))
+const corsOptions = {
+    origin : 'http://localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions))
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is listen at port ${PORT}`);
+    
+})
